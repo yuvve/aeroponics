ed6067ca7e6f567417e9847f52982576bf61c577
yuvve <yuvve.git@pm.me>
2025-12-02 14:24:59 +0100

    Add all alerts

---
+++ OpenHAB/conf/automation/js/alerts/tower-1/temp.js
@@ -0,0 +1,24 @@
+rules.JSRule({
+  name: "Tower 1 temp Level Alert",
+  description: "Updates tower_1_temp_level_alert_status based on settings",
+  triggers: [
+    triggers.ItemStateChangeTrigger('tower_1_upper_temp'),
+    triggers.ItemStateChangeTrigger('tower_1_alerts_toggle'),
+    triggers.ItemStateChangeTrigger('tower_1_low_temp_alert_setting'),
+    triggers.ItemStateChangeTrigger('tower_1_high_temp_alert_setting'),
+  ],
+  execute: () => {
+    const alerts_on = items.getItem('tower_1_alerts_toggle').state === 'ON';
+    const min_temp_level = items.getItem('tower_1_low_temp_alert_setting').numericState;
+    const max_temp_level = items.getItem('tower_1_high_temp_alert_setting').numericState;
+    const temp_level = items.getItem('tower_1_temp').numericState;
+
+    const alertItem = items.getItem('tower_1_temp_alert_status');
+
+    if (alerts_on == 'ON' && (temp_level < min_temp_level || temp_level > max_temp_level)) {
+      alertItem.postUpdate('ON');
+    } else {
+      alertItem.postUpdate('OFF');
+    }
+  }
+});
---
+++ OpenHAB/conf/automation/js/alerts/tower-1/water-pressure.js
@@ -0,0 +1,22 @@
+rules.JSRule({
+  name: "Tower 1 Water Pressure Alert",
+  description: "Updates tower_1_water_pressure_alert_status based on settings",
+  triggers: [
+    triggers.ItemStateChangeTrigger('tower_1_pressure'),
+    triggers.ItemStateChangeTrigger('tower_1_alerts_toggle'),
+    triggers.ItemStateChangeTrigger('tower_1_low_water_pressure'),
+  ],
+  execute: () => {
+    const alerts_on = items.getItem('tower_1_alerts_toggle').state === 'ON';
+    const min_water_pressure = items.getItem('tower_1_low_water_pressure_alert_setting').numericState;
+    const water_pressure = items.getItem('tower_1_pressure').numericState;
+
+    const alertItem = items.getItem('tower_1_water_pressure_alert_status');
+
+    if (alerts_on && water_pressure < min_water_pressure) {
+      alertItem.postUpdate('ON');
+    } else {
+      alertItem.postUpdate('OFF');
+    }
+  }
+});
